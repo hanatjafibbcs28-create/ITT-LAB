88de1377b2582e3587128702b0c70089a7a3011a
Hanat Jafi <hanatjafib_bcs28@mepcoeng.ac.in>
2026-07-15 15:44:44 +0530

    Add Solution class with gcdOfOddEvenSums method
    Implement gcdOfOddEvenSums method in Solution class.

---
+++ folder/leetcode/code/GCD of odd and Even Sums.py
@@ -0,0 +1,7 @@
+class Solution:
+    def gcdOfOddEvenSums(self, n: int) -> int:
+        return n
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.gcdOfOddEvenSums(4))  
+    print(sol.gcdOfOddEvenSums(5)) 
