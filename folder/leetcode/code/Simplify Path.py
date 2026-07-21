a836f8b8c402550f78dbd7928c05b2d6a3cf8ca5
Hanat Jafi <hanatjafib_bcs28@mepcoeng.ac.in>
2026-07-21 14:43:59 +0530

    Add Simplify Path solution implementation

---
+++ folder/leetcode/code/Simplify Path.py
@@ -0,0 +1,13 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        segments = path.split('/')
+        stack = []
+        for segment in segments:
+            if not segment or segment == '.':
+                continue
+            if segment == '..':
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(segment)
+        return '/' + '/'.join(stack)
