262917fb77d43d54ff76c77904a27d1befd1b6e8
Hanat Jafi <hanatjafib_bcs28@mepcoeng.ac.in>
2026-07-21 16:16:45 +0530

    Create  Construct Binary Tree from Inorder and Postorder Traversal.py

---
+++ folder/leetcode/code/Construct Binary Tree from Inorder and Postorder Traversal.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def buildTree(self, inorder, postorder):
+        if not postorder:
+            return None
+            
+        root = TreeNode(postorder[-1])
+        stack = [root]
+        in_idx = len(inorder) - 1
+        
+        for i in range(len(postorder) - 2, -1, -1):
+            curr_val = postorder[i]
+            node = stack[-1]
+            
+            if node.val != inorder[in_idx]:
+                node.right = TreeNode(curr_val)
+                stack.append(node.right)
+            else:
+                while stack and stack[-1].val == inorder[in_idx]:
+                    node = stack.pop()
+                    in_idx -= 1
+                    
+                node.left = TreeNode(curr_val)
+                stack.append(node.left)
+                
+        return root
