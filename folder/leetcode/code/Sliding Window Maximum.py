16683e7721baa3160ac5d79bd7a38227e5983408
Hanat Jafi <hanatjafib_bcs28@mepcoeng.ac.in>
2026-08-04 15:33:10 +0530

    Create  Sliding Window Maximum.py

---
+++ folder/leetcode/code/Sliding Window Maximum.py
@@ -0,0 +1,14 @@
+from collections import deque
+class Solution:
+    def maxSlidingWindow(self, nums: list[int], k: int) -> list[int]:
+        q = deque()  
+        res = []
+        for i, num in enumerate(nums):
+            if q and q[0] < i - k + 1:
+                q.popleft()
+            while q and nums[q[-1]] < num:
+                q.pop()
+            q.append(i)
+            if i >= k - 1:
+                res.append(nums[q[0]])
+        return res
