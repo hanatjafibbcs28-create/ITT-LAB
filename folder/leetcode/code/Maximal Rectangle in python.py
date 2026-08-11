dd957fbd2b7d20f8d27167ef10255b7c4a00e437
Hanat Jafi <hanatjafib_bcs28@mepcoeng.ac.in>
2026-08-11 15:21:23 +0530

    Create Maximal Rectangle in python.py

---
+++ folder/leetcode/code/Maximal Rectangle in python.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maximalRectangle(self, matrix: List[List[str]]) -> int:
+        if not matrix or not matrix[0]: return 0
+        cols = len(matrix[0])
+        heights = [0] * (cols + 1)
+        max_area = 0
+        for row in matrix:
+            for i in range(cols):
+                heights[i] = heights[i] + 1 if row[i] == '1' else 0
+            stack = []
+            for i, h in enumerate(heights):
+                while stack and heights[stack[-1]] > h:
+                    height = heights[stack.pop()]
+                    width = i if not stack else i - stack[-1] - 1
+                    max_area = max(max_area, height * width)
+                stack.append(i)    
+        return max_area
