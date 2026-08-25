9a2db57927a6bbd234a3e0d4ceb53a006ecd64a5
Hanat Jafi <hanatjafib_bcs28@mepcoeng.ac.in>
2026-08-25 15:48:52 +0530

    Create 240. Search a 2D Matrix II.c

---
+++ folder/leetcode/code/240. Search a 2D Matrix II.c
@@ -0,0 +1,20 @@
+#include <stdbool.h>
+bool searchMatrix(int** matrix, int matrixSize, int* matrixColSize, int target) {
+    if (matrixSize == 0 || matrixColSize[0] == 0) {
+        return false;
+    }
+    int m = matrixSize;
+    int n = matrixColSize[0];
+    int row = 0;
+    int col = n - 1;
+    while (row < m && col >= 0) {
+        if (matrix[row][col] == target) {
+            return true;
+        } else if (matrix[row][col] > target) {
+            col--; 
+        } else {
+            row++; 
+        }
+    }
+    return false;
+}
