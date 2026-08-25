aa21de62e3af3c128545669fb8986d18f09e81dd
Hanat Jafi <hanatjafib_bcs28@mepcoeng.ac.in>
2026-08-25 14:40:39 +0530

    Create 69. Sqrt(x).py

---
+++ folder/leetcode/code/69. Sqrt(x).py
@@ -0,0 +1,17 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x < 2:
+            return x
+        left, right = 2, x // 2
+        ans = 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            square = mid * mid
+            if square == x:
+                return mid
+            elif square < x:
+                ans = mid     
+                left = mid + 1 
+            else:
+                right = mid - 1    
+        return ans
