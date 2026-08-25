4c1698dba689eb22de37664d8735f295bc81f0cf
Hanat Jafi <hanatjafib_bcs28@mepcoeng.ac.in>
2026-08-25 14:43:13 +0530

    Create 69. Sqrt(x) 1.c

---
+++ folder/leetcode/code/69. Sqrt(x) 1.c
@@ -0,0 +1,18 @@
+int mySqrt(int x) {
+    if (x < 2) {
+        return x;
+    }
+    int left = 2;
+    int right = x / 2;
+    int ans = 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (mid <= x / mid) {
+            ans = mid;      
+            left = mid + 1;   
+        } else {
+            right = mid - 1;  
+        }
+    }
+    return ans;
+}
