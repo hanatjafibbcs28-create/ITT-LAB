1f98534d6bca9bfea45fc4ac01935222d5eda4b9
Hanat Jafi <hanatjafib_bcs28@mepcoeng.ac.in>
2026-09-15 16:18:00 +0530

    Create 347. Top K Frequent Elements.java

---
+++ folder/leetcode/code/347. Top K Frequent Elements.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        // Step 1: Count the frequency of each element
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        for (int num : nums) {
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+        }
+
+        // Step 2: Create buckets where the index represents frequency
+        List<Integer>[] bucket = new List[nums.length + 1];
+        for (int key : frequencyMap.keySet()) {
+            int frequency = frequencyMap.get(key);
+            if (bucket[frequency] == null) {
+                bucket[frequency] = new ArrayList<>();
+            }
+            bucket[frequency].add(key);
+        }
+
+        // Step 3: Collect the top k frequent elements from the end of the bucket array
+        int[] result = new int[k];
+        int index = 0;
+        
+        for (int pos = bucket.length - 1; pos >= 0 && index < k; pos--) {
+            if (bucket[pos] != null) {
+                for (int num : bucket[pos]) {
+                    result[index++] = num;
+                    if (index == k) {
+                        return result;
+                    }
+                }
+            }
+        }
+        
+        return result;
+    }
+}
