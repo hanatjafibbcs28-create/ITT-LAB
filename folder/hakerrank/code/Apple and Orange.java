78f74f871bd81da19c7a1324f75fe07100e879d9
Hanat Jafi <hanatjafib_bcs28@mepcoeng.ac.in>
2026-09-15 14:47:41 +0530

    Create Apple and Orange.java

---
+++ folder/hakerrank/code/Apple and Orange.java
@@ -0,0 +1,53 @@
+
+import math
+import os
+import random
+import re
+import sys
+#
+# Complete the 'countApplesAndOranges' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER s
+#  2. INTEGER t
+#  3. INTEGER a
+#  4. INTEGER b
+#  5. INTEGER_ARRAY apples
+#  6. INTEGER_ARRAY oranges
+#
+
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    # Calculate landing position for each apple and check if it falls on the house
+    apple_count = sum(1 for d in apples if s <= a + d <= t)
+    
+    # Calculate landing position for each orange and check if it falls on the house
+    orange_count = sum(1 for d in oranges if s <= b + d <= t)
+    
+    # Print the final counts on separate lines
+    print(apple_count)
+    print(orange_count)
+
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    s = int(first_multiple_input[0])
+
+    t = int(first_multiple_input[1])
+
+    second_multiple_input = input().rstrip().split()
+
+    a = int(second_multiple_input[0])
+
+    b = int(second_multiple_input[1])
+
+    third_multiple_input = input().rstrip().split()
+
+    m = int(third_multiple_input[0])
+
+    n = int(third_multiple_input[1])
+
+    apples = list(map(int, input().rstrip().split()))
+
+    oranges = list(map(int, input().rstrip().split()))
+
+    countApplesAndOranges(s, t, a, b, apples, oranges)
